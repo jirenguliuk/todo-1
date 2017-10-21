a7d153304dbc032b41c1ffe524f6b38099ef03d6
jirenguliuk <707529659@qq.com>
2017-10-22 01:03:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+//import 'normalize.css';
+import './reset.css';
+import './App.css';
+
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import UserDialog from './UserDialog';
+import {getCurrentUser, signOut, TodoModel} from './leanCloud';
+import * as localStore from './localStore';
+
+
+ class App extends Component {
+     constructor(props){
+         super(props)
+         this.state = {
+            user: getCurrentUser() || {},
+            newTodo: '',
+            todoList: []
+         }
+             let user = getCurrentUser()
+             if (user) {
+               TodoModel.getByUser(user, (todos) => {
+                 let stateCopy = JSON.parse(JSON.stringify(this.state))
+                 stateCopy.todoList = todos
+                 this.setState(stateCopy)
+               })
+             }
+     }
+        render(){
+            let todos = this.state.todoList
+            .filter((item)=> !item.deleted)
+            .map((item,index)=>{
+              return ( 
+               <li key={index} >
+                <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                 onDelete={this.delete.bind(this)} />
+               </li>
+           )
+        })
+        console.log(todos)
+
+            return(
+                <div className="App">
+                <h1>{this.state.user.username||'我'}的待办
+                  {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+         </h1>
+                    <div className="inputWrapper">
+                       <TodoInput content={this.state.newTodo} 
+                       onChange={this.changeTitle.bind(this)}
+                       onSubmit={this.addTodo.bind(this)} />
+                    </div>
+                    <ol className="todoList">
+                    {todos}
+                    
+                    </ol>
+        {this.state.user.id ? 
+           null : 
+           <UserDialog 
+             onSignUp={this.onSignUpOrSignIn.bind(this)} 
+             onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
+                </div>
+                )
+            }
+    signOut(){
+        signOut()
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.user = {}
+        this.setState(stateCopy)
+   }
+    onSignUpOrSignIn(user){
+        let stateCopy = JSON.parse(JSON.stringify(this.state)) 
+        stateCopy.user = user
+        this.setState(stateCopy)
+   }
+            // componentDidUpdate 会在组件更新之后调用。如果我们默认「组件更新」等价于「数据更新」，那么就可以把 localStore.save('todoList', this.state.todoList) 写在这个钩子里。
+      componentDidUpdate(){
+            //localStore.save('todoList', this.state.todoList)
+   }
+        toggle(e, todo){
+            let oldStatus = todo.status
+           todo.status = todo.status === 'completed' ? '' : 'completed'
+           TodoModel.update(todo, () => {
+          this.setState(this.state)
+        }, (error) => {
+          todo.status = oldStatus
+          this.setState(this.state)
+        })
+           //localStore.save('todoList', this.state.todoList) 
+   }    
+        changeTitle(event){
+            this.setState({
+                newTodo: event.target.value,
+                 todoList: this.state.todoList
+           })
+               // localStore.save('todoList', this.state.todoList)
+        }    
+        addTodo(event){
+            let newTodo = {
+                title: event.target.value,
+                status: null,
+                deleted: false
+           }
+           TodoModel.create(newTodo, (id) => {
+             newTodo.id = id
+                  this.state.todoList.push(newTodo)
+                  this.setState({
+                    newTodo: '',
+                    todoList: this.state.todoList
+                  })
+                }, (error) => {
+                  console.log(error)
+        })
+               // localStore.save('todoList', this.state.todoList)
+    }
+        delete(event, todo){
+            TodoModel.destroy(todo.id, () => {
+                todo.deleted = true
+                this.setState(this.state)
+             })
+
+  }
+ }
+export default App;
